74c54b220a7191d283b19d5c9c8119d6e8eafaa0
Xancake <xanylein@web.de>
2016-06-21 00:40:49 +0200

    update-Methode für einzelne Bytes implementiert

--- BAI5_WP_CP_Praktikum/src/a4/solution/SchaefersChecksum.java
+++ BAI5_WP_CP_Praktikum/src/a4/solution/SchaefersChecksum.java
@@ -1,6 +1,7 @@
 package a4.solution;
 
 import java.util.zip.Checksum;
+
 import a4.example.Utility;
 
 public class SchaefersChecksum implements Checksum {
@@ -23,22 +24,23 @@ public class SchaefersChecksum implements Checksum {
 	
 	@Override
 	public void update(int b) {
-		throw new UnsupportedOperationException("Not implemented");
+		// put next byte at end (according to our "signature thinking" the least significant position)
+		_crc |= ((0xffL & b) << 32);
+		
+		// handle current byte
+		for(int bitPosition=0; bitPosition<8; bitPosition++) {
+			if(0b1 == (0b1 & _crc)) {
+				_crc ^= _polinom;
+			}
+			_crc >>>= 1;
+		}
+//		System.out.printf("%8x%n", _crc);
 	}
 	
 	@Override
 	public void update(byte[] buffer, int offset, int length) {
 		for(int bufferIndex=offset; bufferIndex<length; bufferIndex++) {
-			// put next byte at end (according to our "signature thinking" the least significant position)
-			_crc |= ((0xffL & buffer[bufferIndex]) << 32);
-			
-			// handle current byte
-			for(int bitPosition=0;  bitPosition<8;  bitPosition++) {
-				if(0b1 == (0b1 & _crc)) {
-					_crc ^= _polinom;
-				}
-				_crc = _crc >>> 1;
-			}
+			update(buffer[bufferIndex]);
 		}
 	}
 }
